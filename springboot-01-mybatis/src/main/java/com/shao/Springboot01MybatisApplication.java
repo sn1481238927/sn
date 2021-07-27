753ede3ed6fc2e74df59f3a55f5db1d6edbfc0cf
sn <1481238927@qq.com>
2021-07-27 14:24:56 +0800

    mybatis

---
+++ springboot-01-mybatis/src/main/java/com/shao/Springboot01MybatisApplication.java
@@ -0,0 +1,13 @@
+package com.shao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01MybatisApplication.class, args);
+    }
+
+}
